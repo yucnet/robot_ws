70ea097f831c600e687224e6eb48b1d5893957b0
ZhangEaky <zyk090104@163.com>
2020-02-26 15:54:44 +0800

    2020.2.26

--- src/axis_tf/src/tf.cpp
+++ src/axis_tf/src/tf.cpp
@@ -25,6 +25,7 @@
 #include <dobot/GetPose.h>
 #include <opencvtest/pointQueue.h>
 /*********************VARIBE***********************************/
+#include<unistd.h>
 using namespace std;
 
 class Transform
@@ -43,7 +44,9 @@ tf::TransformBroadcaster marker_position_broadcaster;
 tf::TransformBroadcaster magician_origin_bra;
 tf::TransformBroadcaster dobot_effector_bra;
 
-cv::Matx33d camera_matrix;
+cv::Matx33d camera_matrix_for_estimate;
+Eigen::Matrix3d camera_matrix_for_cal;
+Eigen::Matrix3d inverse_camera_matrix;
 cv::Mat dist_coeffs; 
 
 vector < vector<cv::Point2f> > corners;
@@ -57,17 +60,27 @@ vector< vector<int> > image_points;
 vector<double> tvector;
 vector<double> rvector;
 
+cv::Mat global_paper;
+
+double Zc;
+
 public:
 Transform():it(n)
-{
+{   
+    cout<<getpid()<<endl;
     this->client_pose = n.serviceClient<dobot::GetPose>("dobot/GetPose");
-    this->publisher = this->n.advertise<geometry_msgs::PointStamped>("stampedPoint",1000,this);
+    this->publisher = this->n.advertise<geometry_msgs::PointStamped>("marker_corners_in_the_camera",1000,this);
+    
     this->image_sub_ = it.subscribe("/camera/color/image_raw",1,&Transform::callbackImage,this);
     this->aligned_image_sub = it.subscribe("/camera/aligned_depth_to_color/image_raw",
     1,&Transform::callbackAlignedImage,this);
-    this->camera_matrix << 615.1227416992188, 0.000000,  327.1946716308594,
-                           0.000000, 614.7720336914062,  242.58395385742188, 
-                           0.000000,          0.000000,  1.000000;
+    this->camera_matrix_for_estimate << 615.1227416992188,          0.000000,   327.1946716308594,
+                                                 0.000000, 614.7720336914062,  242.58395385742188, 
+                                                 0.000000,          0.000000,            1.000000;
+
+    this->camera_matrix_for_cal << 615.1227416992188,          0.000000,  327.1946716308594,
+                                            0.000000, 614.7720336914062,  242.58395385742188, 
+                                            0.000000,          0.000000,  1.000000;
 
     this->dist_coeffs = (cv::Mat_<double>(1,5) << 0.000, 0.000, 0.000, 0.000, 0.000);
     this->dictionary = cv::aruco::getPredefinedDictionary(cv::aruco::DICT_6X6_100);   
@@ -81,6 +94,8 @@ void getMarker(cv::Mat& marker_image, vector<cv::Point2f>& marker_center, bool k
 void getMarkerCoordinate(vector < vector<cv::Point2f> >& corners, vector<int>& ids, vector<cv::Point2f>& marker_center);
 void sendMarkerTf(vector<cv::Vec3d>& marker_vecs, vector<cv::Vec3d>& marker_rvecs);
 void sendDobotTf();
+
+void inverseCameraMatrix(double zc);
 };
 int main(int argc, char** argv)
 {
@@ -102,6 +117,7 @@ void Transform::callbackAlignedImage(const sensor_msgs::ImageConstPtr& msg)
             ROS_ERROR("cv_bridge exception: %s", e.what());
             return;
         }
+        cv::Mat paper = cv_ptr->image.clone();
         if(this->corners.size() == 0)
             {
                 cout<<"no corner"<<endl;
@@ -109,14 +125,29 @@ void Transform::callbackAlignedImage(const sensor_msgs::ImageConstPtr& msg)
             }else{
                 for(int i=0;i< this->corners[0].size();i++)
                     {
-                        double d = cv_ptr->image.at<unsigned short>(corners[0][i].x,corners[0][i].y)/1000.00;
-                        cout<<d<<endl;
-                        geometry_msgs::PointStamped point;
-                
+                      
+                        Zc = cv_ptr->image.at<unsigned short>(corners[0][i].x,corners[0][i].y)/1000.00;
+                        cv::putText(this->global_paper,std::to_string(Zc),corners[0][i],1,1.5,cv::Scalar(0,0,255),1,8);
+                        if(Zc == 0)
+                            {
+                                cout<<"XXXXX"<<endl;
+                            }else{
+                            cout<<Zc<<endl;
+                        }
                         
+                        geometry_msgs::PointStamped point;
+                        point.header.frame_id = "camera_color_optical_frame";
+                        point.header.stamp = ros::Time();
+                        Eigen::Vector3d temp = {corners[0][i].x,corners[0][i].y,1};
+                        this->inverseCameraMatrix(Zc);
+                        Eigen::Vector3d camera_axis_point= inverse_camera_matrix*temp;
+                        point.point.x = camera_axis_point[0];
+                        point.point.y = camera_axis_point[1];
+                        point.point.z = Zc;
+                        this->publisher.publish(point);
                     }
             }
-        cv::Mat paper = cv_ptr->image.clone();
+        
        
         cv::imshow("AlignedImage",paper);
         cv::waitKey(1);
@@ -134,9 +165,9 @@ void Transform::callbackImage(const sensor_msgs::ImageConstPtr& msg)
             ROS_ERROR("cv_bridge exception: %s", e.what());
             return;
         }
-        cv::Mat paper = cv_ptr->image.clone();
-        this->getMarker(paper,marker_center,0);
-        cv::imshow("callbackImage",paper);
+        this->global_paper = cv_ptr->image.clone();
+        this->getMarker(global_paper,marker_center,0);
+        cv::imshow("callbackImage",global_paper);
         cv::waitKey(1);
     }
 
@@ -152,12 +183,12 @@ void Transform::getMarker(cv::Mat& marker_image, vector<cv::Point2f>& marker_cen
                     // cout<<"角点:"<<endl;
                     for(int i = 0;i<corners[0].size();i++)
                         {
-                            //cout<<"("<<corners[0][i].x<<","<<corners[0][i].y<<")"<<endl;
+                            cv::circle(marker_image,corners[0][i],2,cv::Scalar(255,0,0),5);
                         }
                       cv::aruco::drawDetectedMarkers(marker_image, corners, ids);
 
-                this->getMarkerCoordinate(corners,ids,marker_center);
-                    cv::aruco::estimatePoseSingleMarkers(corners,0.145, this->camera_matrix, dist_coeffs, rvecs, tvecs);
+                    this->getMarkerCoordinate(corners,ids,marker_center);
+                    cv::aruco::estimatePoseSingleMarkers(corners,0.076, this->camera_matrix_for_estimate, dist_coeffs, rvecs, tvecs);
                     
                     if(rvecs.empty()&&tvecs.empty())
                         {
@@ -165,7 +196,7 @@ void Transform::getMarker(cv::Mat& marker_image, vector<cv::Point2f>& marker_cen
                         }
                     else
                         {
-                            cv::aruco::drawAxis(marker_image, this->camera_matrix, dist_coeffs ,rvecs, tvecs, 0.1); 
+                            cv::aruco::drawAxis(marker_image, this->camera_matrix_for_estimate, dist_coeffs ,rvecs, tvecs, 0.1); 
                         }  
                     sendMarkerTf(rvecs,tvecs);
                     }
@@ -185,8 +216,8 @@ void Transform::getMarkerCoordinate(vector < vector<cv::Point2f> >& corners, vec
                 center += corners[0][i];
             }
         center /= 4.0;
-        marker_center.push_back(center);
-        cout<<marker_center[0].x<<","<<marker_center[1].y<<endl;
+        
+       // cout<<marker_center[0].x<<","<<marker_center[1].y<<endl;
     }
 
 void Transform::sendMarkerTf(vector<cv::Vec3d>& marker_rvecs,vector<cv::Vec3d>& marker_tvecs)                 
@@ -226,7 +257,17 @@ void Transform::sendDobotTf()
 
         tf::Vector3 tf_tvecs(0.080,0.280, 0.00);
         tf::Transform transform(tf_rotated_matrix, tf_tvecs);
-       
+        
         this->magician_origin_bra.sendTransform(tf::StampedTransform(transform, ros::Time::now(),"world","magician_origin"));
         //this->sendDobotEffectorTF();
     }
+
+void Transform::inverseCameraMatrix(double zc)
+{
+    this->inverse_camera_matrix = this->camera_matrix_for_cal.inverse(); 
+    this->inverse_camera_matrix(0,0) *=  zc;
+    this->inverse_camera_matrix(0,2) *=  zc;
+    this->inverse_camera_matrix(1,1) *=  zc;
+    this->inverse_camera_matrix(1,2) *=  zc;
+    this->inverse_camera_matrix(2,2) *=  zc;
+}
