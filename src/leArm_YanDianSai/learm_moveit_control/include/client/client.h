d7c90165e1c62db75f78771d1ffeeb4bc5a51c35
ZhangEaky <zyk090104@163.com>
2020-05-04 21:25:51 +0800

    failed to plan

--- src/leArm_YanDianSai/learm_moveit_control/include/client/client.h
+++ src/leArm_YanDianSai/learm_moveit_control/include/client/client.h
@@ -2,10 +2,12 @@
 #include <iostream>
 #include <vector>
 #include <actionlib/client/simple_action_client.h>
+#include <moveit_msgs/RobotState.h>
 #include <control_msgs/FollowJointTrajectoryAction.h>
 #include <trajectory_msgs/JointTrajectory.h>
 #include <geometry_msgs/Pose.h>
 #include <moveit/move_group_interface/move_group_interface.h>
+#include <tf/transform_broadcaster.h>
 using namespace std;
 typedef actionlib::SimpleActionClient<control_msgs::FollowJointTrajectoryAction> client;
 
@@ -25,7 +27,10 @@ public:
     trajectory_msgs::JointTrajectory traj;
     control_msgs::JointTolerance tol;
     moveit::planning_interface::MoveGroupInterface group;
+    moveit::planning_interface::MoveGroupInterface::Plan myPlan;
+    moveit_msgs::RobotTrajectory trajectory;
     vector<string> joints_name;
+    
 
 
 public:
@@ -35,5 +40,7 @@ public:
     void feedbackCb(const control_msgs::FollowJointTrajectoryFeedbackConstPtr& feedback);
     void doneCb(const actionlib::SimpleClientGoalState& state,
         const control_msgs::FollowJointTrajectoryResultConstPtr& result);
-    void CartesianPath();
+    void CartesianPath(double x, double y, double z);
+    void goToPoseGoal();
+    void goToJointState();
 };
